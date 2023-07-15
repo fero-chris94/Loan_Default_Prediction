c545b267066946ed5741b12473673c8affe1d0de
CHRISTOPHER ANDREW EREFOROKUMA <120226446+fero-chris94@users.noreply.github.com>
2023-07-15 17:46:35 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -64,23 +64,31 @@ columns=['person_age', 'person_income', 'person_home_ownership', 'person_emp_len
 
 st.title('LOAN DEFAULT PREDICTOR')
 # st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
-st.header('Enter the characteristics of applicant:')
-person_age = st.number_input('Age of Applicant:', min_value=0.0, max_value=120.0, value=1.0)
-person_income = st.number_input('Income of Applicant:', min_value=0.0, max_value=1000000.0, value=1.0)
-person_home_ownership = st.selectbox('Type of Residence:', ['RENT', 'MORTGAGE', 'OWN', 'OTHER'])
-person_emp_length = st.number_input('Years of Employment:', min_value=0.0, max_value=30.0, value=1.0)
-loan_intent = st.selectbox('LOAN INTENT:', ['EDUCATION', 'MEDICAL', 'VENTURE', 'PERSONAL', 'DEBTCONSOLIDATION', 'HOMEIMPROVEMENT'])
-loan_grade = st.selectbox('LOAN GRADE:', ['A', 'B', 'C', 'D', 'E', 'F', 'G'])
-loan_amnt = st.number_input('Loan Amount:', min_value=0.0, max_value=1000000.0, value=1.0)
-loan_int_rate = st.number_input('Interest Rate:', min_value=0.0, max_value=100.0, value=0.01)
-loan_percent_income = st.number_input('Percentage of loan to Income', min_value=0.0, max_value=1.0, value=1.0)
-cb_person_default_on_file = st.selectbox('Previous History of Default:', ['N','Y'])
-cb_person_cred_hist_length = st.number_input('Length of Credit History (In Years)', min_value=0.0, max_value=50.0, value=1.0)
+st.header('Enter the characteristics of the applicant:')
 
+# Split the input boxes into two columns
+col1, col2 = st.columns(2)
+
+# Input boxes for the first column
+with col1:
+    person_age = st.number_input('Age of Applicant:', min_value=0.0, max_value=120.0, value=1.0)
+    person_income = st.number_input('Income of Applicant:', min_value=0.0, max_value=1000000.0, value=1.0)
+    person_home_ownership = st.selectbox('Type of Residence:', ['RENT', 'MORTGAGE', 'OWN', 'OTHER'])
+    person_emp_length = st.number_input('Years of Employment:', min_value=0.0, max_value=30.0, value=1.0)
+    loan_intent = st.selectbox('Loan Intent:', ['EDUCATION', 'MEDICAL', 'VENTURE', 'PERSONAL', 'DEBTCONSOLIDATION', 'HOMEIMPROVEMENT'])
+
+# Input boxes for the second column
+with col2:
+    loan_grade = st.selectbox('Loan Grade:', ['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+    loan_amnt = st.number_input('Loan Amount:', min_value=0.0, max_value=1000000.0, value=1.0)
+    loan_int_rate = st.number_input('Interest Rate:', min_value=0.0, max_value=100.0, value=0.01)
+    loan_percent_income = st.number_input('Percentage of Loan to Income', min_value=0.0, max_value=100.0, value=0.01)
+    cb_person_default_on_file = st.selectbox('Previous History of Default:', ['N','Y'])
+    cb_person_cred_hist_length = st.number_input('Length of Credit History (In Years)', min_value=0.0, max_value=50.0, value=1.0)
 
 if st.button('Predict Default'):
     loan_status = predict(person_age, person_income, person_home_ownership, person_emp_length, loan_intent, loan_grade, 
-loan_amnt, loan_int_rate, loan_percent_income, cb_person_default_on_file, cb_person_cred_hist_length)
+                          loan_amnt, loan_int_rate, loan_percent_income, cb_person_default_on_file, cb_person_cred_hist_length)
     result = 'YES' if loan_status[0] == 1 else 'NO'
     st.success(f'Application Result: {result}')
-    
+
